f925dcfe3d0a233b982085b3e68ad4f7e36915ae
Moinulilm10 <moinulilm10@gmail.com>
2023-05-16 20:26:09 +0600

    header file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
       <div>
-        <h1>Vite + React</h1>
+        <Header />
       </div>
     </>
   );

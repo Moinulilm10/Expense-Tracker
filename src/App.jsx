a8a88c5d608bc6e13d6ac67bded05f509f29172e
Moinulilm10 <moinulilm10@gmail.com>
2023-05-18 22:42:40 +0600

    balance component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 
 function App() {
   return (
     <>
       <div>
         <Header />
+        <Balance />
       </div>
     </>
   );

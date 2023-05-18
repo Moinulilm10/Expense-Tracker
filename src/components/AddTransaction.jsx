cedb86f1b04cb9173362deae12a157ecfa1c67de
Moinulilm10 <moinulilm10@gmail.com>
2023-05-19 00:17:30 +0600

    id attribute remove from all component

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -7,14 +7,14 @@ export const AddTransaction = () => {
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input type="text" placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br />
             (negative - expense, positive - income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount..." />
+          <input type="number" placeholder="Enter amount..." />
         </div>
         <button className="btn">Add transaction</button>
       </form>
